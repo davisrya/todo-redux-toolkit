0068fc413e4f835a95eeaab7cd3d535c37268f47
Kija Brian <kijabryan@gmail.com>
2025-02-27 23:57:04 +0300

    unistall unwanted tools

--- src/store/store1.js
+++ src/store/store1.js
@@ -6,6 +6,8 @@ const store1 = configureStore({
 	reducer: {
 		todos: todoReducer,
 		theme: themeReducer,
+		// eslint-disable-next-line no-undef
+		devTools: process.env.NODE_ENV !== 'production', // Enables Redux DevTools
 	},
 });
 
